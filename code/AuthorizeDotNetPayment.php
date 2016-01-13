abae0e58a6e9a104b1b1866fbcd783f432e736b1
Nicolaas <github@sunnysideup.co.nz>
2016-01-14 12:57:19 +1300

    API Fix

--- code/AuthorizeDotNetPayment.php
+++ code/AuthorizeDotNetPayment.php
@@ -236,7 +236,7 @@ class AuthorizeDotNetPayment extends EcommercePayment {
 			$controller = new ContentController($page);
 			//Requirements::block(THIRDPARTY_DIR."/jquery/jquery.js");
 			//Requirements::javascript(Director::protocol()."ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
-			return new Payment_Processing($controller->renderWith('PaymentProcessingPage'));
+			return EcommercePayment_Processing::create($controller->renderWith('PaymentProcessingPage'));
 		}
 		else {
 			$page->Title = 'Sorry, Authorize.Net can not be contacted at the moment ...';
